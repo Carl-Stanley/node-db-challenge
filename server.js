a5a7bf87df2a19cc06c5fd01c8ac6e3fd5f1ef58
Carl-Stanley <cds.beml@gmail.com>
2019-11-23 10:56:05 -0700

    Commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const resourcesRouter = require('./api/routes/resourcerouter.js')
 const projectRouter = require('./api/routes/projectrouter.js')
 
 
+
 const db = require('./data/db-Config');
 
 const server = express();
@@ -15,6 +16,7 @@ server.use(express.json());
 server.use('/api/projects', projectRouter)
 
 server.use('/api/resources', resourcesRouter)
+
 
 
 module.exports = server;
